d648a8a1224e4ac284f728a96f3ba200a319dfd4
Ws-Peroth <a01033056809@gmail.com>
2021-04-04 23:41:42 +0900

    Auth UI Finish
    Auth UI Finish

---
+++ Firebase Auth And Database/Assets/Scripts/AuthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+
+
+public class AuthManager : MonoBehaviour
+{
+    public static AuthManager authManager;
+    public AuthUI ui;
+
+    public GameObject signUpErrorToast;
+    public GameObject loginErrorToast;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        if (authManager == null)
+        {
+            authManager = this;
+        }
+    }
+
+    public void DoLogin(string email, string password)
+    {
+
+    }
+
+    public void DoSignUp(string email, string password, string name)
+    {
+        ui.SignUpPageToLoginPage();
+    }
+
+}
---
+++ Firebase Auth And Database/Assets/Scripts/AuthUI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthUI : MonoBehaviour
+{
+    public GameObject backGround;
+    public GameObject startPage;
+    public GameObject loginPage;
+    public GameObject signUpPage;
+
+    public InputField loginEmail;
+    public InputField loginPassword;
+
+    public InputField signUpEmail;
+    public InputField signUpPassword;
+    public InputField signUpUserName;
+
+    public int currentPageNumber;
+    // 0 : Start Page, 1 : Login Page, 2 : SignUp Page
+
+
+    void Start()
+    {
+        currentPageNumber = 0;
+
+        backGround.SetActive(true);
+        startPage.SetActive(true);
+        loginPage.SetActive(false);
+        signUpPage.SetActive(false);
+    }
+
+    public void StartLoginButtonDown() // Start 페이지의 LoginButton 실행 코드
+    {
+        startPage.SetActive(false);
+        loginPage.SetActive(true);
+        signUpPage.SetActive(false);
+    }
+
+    public void StartSignUpButtonDown() // Start 페이지의 SignUpButton 실행 코드
+    {
+        startPage.SetActive(false);
+        loginPage.SetActive(false);
+        signUpPage.SetActive(true);
+    }
+
+    public void SignUpLoginButtonDown() // SignUp 페이지의 LoginButton 실행 코드
+    {
+        startPage.SetActive(false);
+        loginPage.SetActive(true);
+        signUpPage.SetActive(false);
+    }
+
+    public void SignUpEnterButtonDown() // SignUp 페이지의 Enter 실행 코드
+    {
+        AuthManager.authManager.DoSignUp(signUpEmail.text, signUpPassword.text, signUpUserName.text);
+    }
+
+    public void LoginSignUpButtonDown() // Login 페이지의 SignUp 실행 코드
+    {
+        startPage.SetActive(false);
+        loginPage.SetActive(false);
+        signUpPage.SetActive(true);
+    }
+
+    public void LoginEnterButtonDown() // Login 페이지의 Enter 실행 코드
+    {
+        AuthManager.authManager.DoLogin(signUpEmail.text, signUpPassword.text);
+    }
+
+    public void SignUpPageToLoginPage()
+    {
+        startPage.SetActive(false);
+        loginPage.SetActive(true);
+        signUpPage.SetActive(false);
+    }
+
+}
